0a76538222cc033e94285ab712f224a25f781791
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2023-01-12 21:49:17 -0800

    added html and javascript to make average function work

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HW2
 Prog209 first Github Home Work
+Team members are Eyob Worku and Kurt Friedrich
+
--- main.js
+++ main.js
@@ -2,7 +2,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     document.getElementById("button1").addEventListener("click", function () {
 
-        document.getElementById("textbox1").value = "success";
+        let num1 = parseFloat (document.getElementById("textbox1").value);
+        let num2 = parseFloat (document.getElementById("textbox2").value);
+        let num3 = parseFloat (document.getElementById("textbox3").value);
+        let answer = (num1 + num2 + num3)/3
+        console.log(answer);
+        document.getElementById("output").innerHTML = answer;
+
+
     });
 });
 
